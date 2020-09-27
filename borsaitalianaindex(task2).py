648a467b37f81dc84c166dd05a0e93ac521d7789
habubey <71140097+habubey@users.noreply.github.com>
2020-09-27 23:58:16 +0300

    Update borsaitalianaindex(task2).py

--- borsaitalianaindex(task2).py
+++ borsaitalianaindex(task2).py
@@ -1,6 +1,7 @@
 import pandas as pd
 from selenium import webdriver
 import time
+import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,6 +18,7 @@ import urllib3
 import zipfile
 import csv
 
+# sayfa yine değişmiyor csv yapılacak
 
 option = webdriver.ChromeOptions()
 
@@ -24,9 +26,8 @@ browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=op
 browser.set_window_size(444,444)
 
 
-#İÇERİKleri ben aldım ama asıl index sayfasının loopunu alamadım https://www.borsaitaliana.it/borsa/azioni/listino-a-z.html?initial=A&lang=en bu sayfanın hem page numberlı hem
-#hem a dan z ye gidiyor teleborsaindextask2 dosyasıyla benzer page var bunda bir tek
-# arr_urls = []
+#LOOP YAPILACAK LİNKİN ÖNCEKİ SAYFASINA, İÇERİKLER ALINA BİLİYOR
+arr_urls = []
 # arr_stocknames = []
 # arr_stock_indexs = []
 # arr_isin_codes = []
@@ -34,35 +35,106 @@ browser.set_window_size(444,444)
 # arr_sectors = []
 # arr_ftse_indices = []
 
+currentitem = 3
+currentpage = 1
+arr_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 
-browser.get("https://www.borsaitaliana.it/borsa/azioni/scheda/IT0005395071.html?lang=en")
 
-Name = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[3]/article/div/div/div[1]/h1/a').text
-print("Stock Name: " + Name)
-print("*")
-Stock = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[5]/td[2]/span').text
-print("Stock Index: " + Stock)
-print("*")
-Isin = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[4]/td[2]/span').text
-print("Isin code: " + Isin)
-print("*")
-Market = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[1]/table/tbody/tr[6]/td[2]').text
-print("Market/Segment: " + Market)
-print("*")
-try:
-    Sector = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[6]/td[2]').text
-    print("Sector: " + Sector)
-except:
-    print("No Sector Found")
 
-print("*")
-try:
-    Ftse = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[3]/table/tbody/tr/td[2]/span').text
-    print("Ftse Indices: " + Ftse)
-except:
-    print("No FTSE Indices Found")
 
+cur_char = 0
+cur_pagination = 1
+
+while cur_char < len(arr_letters):
+    time.sleep(3)
+    #ilk harfi yükle
+    browser.get("https://www.borsaitaliana.it/borsa/azioni/listino-a-z.html?initial=" + str(
+        arr_letters[cur_char]) + "&lang=en&page=" + str(cur_pagination))
+    #harfte 1den fazla sayfa var mı bak
+    pages_len = len(
+        browser.find_elements_by_xpath(
+            "//*[@id=\"fullcontainer\"]/main/section/div[3]/article[1]/div[2]/div[2]/div/ul/*")) - 1
+    print("Pages Length:" + str(pages_len))
+    # şu anda zaten ilk sayfada
+
+    total_urls = len(browser.find_elements_by_xpath(
+        "//*[@id=\"fullcontainer\"]/main/section/div[3]/article[1]/div[2]/div[1]/div[2]/table/tbody/*")) - 2
+    print("Total URLS: " + str(total_urls))
+    current_row = 1
+    while current_row <= total_urls:
+        stock_url = browser.find_element_by_xpath(
+            "//*[@id=\"fullcontainer\"]/main/section/div[3]/article[1]/div[2]/div[1]/div[2]/table/tbody/tr[" + str(
+                current_row + 2) + "]/td[1]/article/div[1]/a").get_attribute('href')
+        arr_urls.append(stock_url)
+        print("Stock URL: " + stock_url)
+        current_row += 1
+
+    if pages_len <= -1:
+        pages_len = 1
+        #gerçi gerek yok herhalde ya kaçıncı sayfa tek ona bakak bi rR
+    a = 0
+    #1den fazla sayfa varsa diğerlerini dolaş
+    while a < pages_len - 1:
+
+        time.sleep(3)
+        #1. sayfayı bir daha yüklemesini engelle
+        if (cur_pagination < pages_len):
+            cur_pagination += 1
+        browser.get("https://www.borsaitaliana.it/borsa/azioni/listino-a-z.html?initial=" + str(
+            arr_letters[cur_char]) + "&lang=en&page=" + str(cur_pagination))
+
+        a += 1
+        total_urls = len(browser.find_elements_by_xpath("//*[@id=\"fullcontainer\"]/main/section/div[3]/article[1]/div[2]/div[1]/div[2]/table/tbody/*")) - 2
+        print("Total URLS: " + str(total_urls))
+
+        current_row = 1
+        while current_row <= total_urls:
+            stock_url = browser.find_element_by_xpath("//*[@id=\"fullcontainer\"]/main/section/div[3]/article[1]/div[2]/div[1]/div[2]/table/tbody/tr["+ str(current_row + 2) +"]/td[1]/article/div[1]/a").get_attribute('href')
+            arr_urls.append(stock_url)
+            print("Stock URL: " + stock_url)
+            current_row += 1
+
+    #1 ise alta yeni kod ekle
+
+    #sayfa kısmını sıfırla
+    cur_pagination = 1
+    #diğer harflerden devam et
+    cur_char += 1
+
+print("ALL URLS SIZE: " + str(len(arr_urls)))
+
+
+
+# --------------------------------------------------------------------------------------------------------------------
+for x in arr_urls:
+    browser.get(x)
+    time.sleep(6)
+
+    Name = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[3]/article/div/div/div[1]/h1/a').text
+    print("Stock Name: " + Name)
+    print("*")
+    Stock = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[5]/td[2]/span').text
+    print("Stock Index: " + Stock)
+    print("*")
+    Isin = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[4]/td[2]/span').text
+    print("Isin code: " + Isin)
+    print("*")
+    Market = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[1]/table/tbody/tr[6]/td[2]').text
+    print("Market/Segment: " + Market)
+    print("*")
+    try:
+        Sector = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[2]/table/tbody/tr[6]/td[2]').text
+        print("Sector: " + Sector)
+    except:
+        print("No Sector Found")
+        print("*")
+    try:
+        Ftse = browser.find_element_by_xpath('//*[@id="fullcontainer"]/main/section/div[6]/article/div/div[3]/div[3]/table/tbody/tr/td[2]/span').text
+        print("Ftse Indices: " + Ftse)
+    except:
+        print("No FTSE Indices Found")
+#--------------------------------------------------------------------------------------
 # arr_stocknames.append(str(Name))
 # arr_stock_indexs.append(str(Stock))
 # arr_isin_codes.append(str(Isin))
